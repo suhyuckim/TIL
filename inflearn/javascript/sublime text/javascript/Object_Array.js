8f75dfe643856deaec699e898db319dd268d5f11
suhyuckim <57027460+suhyuckim@users.noreply.github.com>
2020-07-06 17:25:45 +0900

    Add files via upload

---
+++ inflearn/javascript/sublime text/javascript/Object_Array.js
@@ -0,0 +1,5 @@
+var array = [
+	apple,
+	orange,
+	strawberry,
+]
--- inflearn/javascript/sublime text/javascript/Word_Chain.js
+++ inflearn/javascript/sublime text/javascript/Word_Chain.js
@@ -2,19 +2,26 @@ var body = document.body;
 var 단어 = document.createElement("div");
 단어.textContent = "제로초";
 document.body.append(단어);
+var 폼 = document.createElement("form");
+document.body.append(폼);
 var 입력창 = document.createElement("input");
-document.body.append(입력창);
+폼.append(입력창);
 var 버튼 = document.createElement("button");
-document.body.append(버튼);
+폼.append(버튼);
 버튼.textContent = "입력";
 var 결과창 = document.createElement("div");
 document.body.append(결과창);
-버튼.addEventListener("click", function callback(){
+폼.addEventListener("submit", function callback(이벤트){
+	이벤트.preventDefault();
 	if(단어.textContent[단어.textContent.length - 1] === 입력창.value[0]){
 		결과창.textContent = "정답입니다.";
 		단어.textContent = 입력창.value;
+		입력창.value="";
+		입력창.focus();
 	} else {
 		결과창.textContent = "틀렸습니다.";
+		입력창.value="";
+		입력창.focus();
 	}
 });
 
