c9c8136316fc408d827f91b06cafc4565704a9ee
suhyuckim <57027460+suhyuckim@users.noreply.github.com>
2020-07-15 17:12:50 +0900

    Add files via upload

--- inflearn/javascript/sublime text/javascript/숫자야구.js
+++ inflearn/javascript/sublime text/javascript/숫자야구.js
@@ -1,11 +1,14 @@
 var 바디 = document.body;
+var 숫자후보;
+var 숫자배열;
 
-var 숫자후보 = [1,2,3,4,5,6,7,8,9];
-var 숫자배열 = [];
-
-for(var i=0; i<4; i++){
+function 숫자뽑기(){
+	숫자후보 = [1,2,3,4,5,6,7,8,9];
+	숫자배열 = [];
+	for(var i=0; i<4; i++){
 	var 뽑은것 =숫자후보.splice(Math.floor(Math.random() * (9-i)), 1)[0];
 	숫자배열.push(뽑은것);
+	}
 }
 
 console.log(숫자배열);
@@ -21,6 +24,7 @@ var 버튼 = document.createElement("button");
 버튼.textContent = "입력";
 폼.append(버튼);
 
+var 틀린회수 = 0;
 폼.addEventListener("submit", function callback(이벤트){
 	이벤트.preventDefault();
 	var 답 : = 입력창.value;
@@ -29,20 +33,30 @@ var 버튼 = document.createElement("button");
 		결과.textContent = "홈런";
 		입력창.value;
 		입력창.focus();
-		숫자후보 = [1,2,3,4,5,6,7,8,9];
-		숫자배열 = [];
-		for(var i=0; i<4; i++){
-			var 뽑은것 =숫자후보.splice(Math.floor(Math.random() * (9-i)), 1)[0];
-			숫자배열.push(뽑은것);
-		}
+		숫자뽑기();
+		틀린회수 = 0;
 	}else{
 		var 답배열 = 답.split();
 		var 스트라이크 = 0;
 		var 볼 = 0;
-		for(var i=0; i<3; i++){
-			if(답배열[i] === 숫자배열[i]){
+		틀린회수 += 1;
+		if(틀린회수 > 10) {
+			결과.textContent = "10번 넘게 틀리면 실패 답은"+숫자배열.join(',')+"였습니다.";
+			입력창.value;
+			입력창.focus();
+			숫자뽑기();
+			틀린회수 = 0;
+		}else{
+			for(var i=0; i<3; i++){
+			if(Number(답배열[i]) === 숫자배열[i]){
 				스트라이크++;
+			}else if(숫자배열.indexO(Number(답배열[i])) > -1){
+				볼++;
 			}
+		}
+		결과.textContent = 스트라이크 + "스트라이크" + 볼 + "볼입니다.";
+		입력창.value;
+		입력창.focus();
 		}
 	}
 });
