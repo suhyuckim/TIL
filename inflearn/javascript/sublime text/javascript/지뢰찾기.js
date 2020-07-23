405d7032e882b65961e4ba0e501e0a406fad8abc
suhyuckim <57027460+suhyuckim@users.noreply.github.com>
2020-07-23 21:24:06 +0900

    Add files via upload

---
+++ inflearn/javascript/sublime text/javascript/지뢰찾기.js
@@ -0,0 +1,44 @@
+document.querySelector("#exec").addEventListener("click", function(){
+	var hor = parseInt(document.querySelector("#hor").value);
+	var ver = parseInt(document.querySelector("#ver").value);
+	var mine = parseInt(document.querySelector("#mine").value);
+	console.log(hor,ver,mine);
+
+	//지뢰 위치 뽑기
+	var 후보군 =  Array(hor * ver)
+		.fill()
+		.map(function(요소, 인덱스) {
+		return 인덱스 + 1;
+	});
+	console.log(후보군);
+
+	var 셔플 = [];
+	while(후보군.length > 80){
+		var 이동값 = 후보군.splice(Math.floor(Math.random() * 후보군.length),1)[0];
+		셔플.push(이동값);
+	}
+	console.log(셔플);
+
+	//지뢰 테이블 만들기
+	var dataset = [];
+	var tbody = document.querySelector("#table tbody");
+	for(var i=0; i<ver; i++){
+		var arr = [];
+		dataset.push(arr);
+		var tr = document.createElement("tr");
+		for(var j=0; j<hor; j++){
+			arr.push(1);
+			var td = document.createElement("td");
+			tr.appendChild(td);
+		}
+		tbody.appendChild(tr);
+	}
+	//지뢰 심기
+	for(var k=0; k<셔플.length; k++){
+		var 세로 = Math.floor(셔플[k] / 10);
+		var 가로 = 셔플[k] % 10;
+		console.log(세로, 가로);
+		tbody.children[세로].children[가로].textContent = "X";
+		dataset[세로][가로] = "X";
+	}
+});
