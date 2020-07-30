af6654f176a1a79f54567672bafab5c41bff2436
suhyuckim <57027460+suhyuckim@users.noreply.github.com>
2020-07-30 16:56:39 +0900

    Add files via upload

--- inflearn/javascript/sublime text/javascript/지뢰찾기.js
+++ inflearn/javascript/sublime text/javascript/지뢰찾기.js
@@ -1,6 +1,7 @@
 var tbody = document.querySelector("#table tbody");
 var dataset = [];
 document.querySelector("#exec").addEventListener("click", function(){
+	tbody.innerHTML = ""; //tbody 내부태그들을 다 지워버림
 	var hor = parseInt(document.querySelector("#hor").value);
 	var ver = parseInt(document.querySelector("#ver").value);
 	var mine = parseInt(document.querySelector("#mine").value);
@@ -35,10 +36,39 @@ document.querySelector("#exec").addEventListener("click", function(){
 				var 부모tbody = e.currentTarget.parentNode.parentNode;
 				var 줄 = Array.prototype.indexOf.call(부모tr.children, e.currentTarget);
 				var 칸 = Array.prototype.indexOf.call(부모tbody.children, 부모tr);
-				console.log(부모tr, 부모tbody, e.currentTarget, 줄, 칸);
-				e.currentTarget.textContent = "!";
-				dataset[줄][칸] = "!";
+				if(e.currentTarget.textContent === "" || e.currentTarget.textContent === "X"){
+					e.currentTarget.textContent = "!";
+				}else if(e.currentTarget.textContent === "!"){
+					e.currentTarget.textContent = "?";
+				}else if(e.currentTarget.textContent === "?"){
+					if(dataset[줄][칸] === 1){
+						e.currentTarget.textContent = "";						
+					}else if(dataset[줄][칸] === "X"){
+						e.currentTarget.textContent = "X";
+					}
+				}				
 			});
+			td.addEventListener("click", function(e){
+				//클릭했을때 주변 지뢰 개수
+				var 부모tr = e.currentTarget.parentNode;
+				var 부모tbody = e.currentTarget.parentNode.parentNode;
+				var 줄 = Array.prototype.indexOf.call(부모tr.children, e.currentTarget);
+				var 칸 = Array.prototype.indexOf.call(부모tbody.children, 부모tr);
+				if(dataset[줄][칸] === "X"){
+					e.currentTarget.textContent = "펑";
+				}else{
+					var 주변 = [dataset[줄][칸-1], dataset[줄][칸+1]];
+					if(dataset[줄-1]){
+						주변 = 주변.concat([dataset[줄-1][칸-1], dataset[줄-1][칸], dataset[줄-1][칸+1]]);
+					}
+					if(dataset[줄+1]){
+						주변 = 주변.concat([dataset[줄+1][칸-1], dataset[줄+1][칸], dataset[줄+1][칸+1]]);
+					}
+					e.currentTarget.textContent = 주변.filter(function(v){
+						return v === "X";
+					}).length;
+				}
+			});			
 			tr.appendChild(td);
 		}
 		tbody.appendChild(tr);
